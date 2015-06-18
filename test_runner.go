730e93145cd723dbf41b661b04b2f6d353729bb2
Josselin Auguste <jauguste@iblop.net>
2015-06-18 14:09:15 +0200

    Add support for rake test runner

--- test_runner.go
+++ test_runner.go
@@ -1,15 +1,27 @@
 package sheepit
 
 import (
+	"os"
 	"os/exec"
+	"path"
 )
 
 func RunTests(project Project) ([]byte, bool) {
-	command := exec.Command("./run_tests.sh")
-	command.Dir = project.Path()
+	command := getRunnerCommand(project.Path())
 	output, err := command.Output()
 	if err != nil {
 		return output, false
 	}
 	return output, true
+}
+
+func getRunnerCommand(projectPath string) *exec.Cmd {
+	var command *exec.Cmd
+	if _, err := os.Stat(path.Join(projectPath, "run_tests.sh")); err == nil {
+		command = exec.Command("./run_tests.sh")
+	} else if _, err := os.Stat(path.Join(projectPath, "Rakefile")); err == nil {
+		command = exec.Command("rake", "test")
+	}
+	command.Dir = projectPath
+	return command
 }
